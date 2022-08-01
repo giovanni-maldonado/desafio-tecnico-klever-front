ef51cb3e008a58f494bd4ef37a5fc21e38795773
Giovanni Maldonado <giovannitadeu08@gmail.com>
2022-08-01 17:21:27 -0300

    feat: configurações iniciais e limpando react

---
+++ wallet-app/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-content">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+  )
+}
