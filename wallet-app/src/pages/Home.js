c8e1495252546d22d08546a9dcc2d08acd39becb
Giovanni Maldonado <giovannitadeu08@gmail.com>
2022-08-01 18:26:55 -0300

    feat: estrutura basica da Home

--- wallet-app/src/pages/Home.js
+++ wallet-app/src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import '../styles/HomeStyle.css'
+import AddButton from '../components/AddButton'
 
 export default function Home() {
   return (
     <div className="home-content">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>klever</h1>
+      <div>
+        <h2>Wish Wallet</h2>
+        <AddButton />
       </div>
+      <ul>
+        Tokens
+      </ul>
+    </div>
   )
 }
