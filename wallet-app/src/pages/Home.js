daf174796ff432cad15b493ffb9859c984affdc4
Giovanni Maldonado <giovannitadeu08@gmail.com>
2022-08-01 19:13:48 -0300

    feat: estilizando ícones e arrumando posição

--- wallet-app/src/pages/Home.js
+++ wallet-app/src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { ReactComponent as LogoIcon } from '../assets/logo.svg'
 import '../styles/HomeStyle.css'
 import AddButton from '../components/AddButton'
+import { ReactComponent as StarIcon } from '../assets/shooting-star.svg'
 
 export default function Home() {
   return (
     <div className="home-content">
-      <h1>klever</h1>
-      <div>
+      <LogoIcon className="icon-logo"/>
+      <div className="wish-and-button">
+        <StarIcon className="star-icon" fill="#EEFB40"/>
         <h2>Wish Wallet</h2>
         <AddButton />
       </div>
