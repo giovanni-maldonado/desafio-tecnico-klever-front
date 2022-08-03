4c818014c5b0e3d6580ae7530e73d5f266783b9e
Giovanni Maldonado <giovannitadeu08@gmail.com>
2022-08-03 14:48:27 -0300

    feat: estrutura basica pagina de adicionar um token

---
+++ wallet-app/src/pages/AddTokenPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/AddTokenPageStyle.css'
+import BackButton from '../components/BackButton'
+import Token from '../components/Token'
+import { ReactComponent as StarIcon } from '../assets/shooting-star.svg'
+import { ReactComponent as LogoIcon } from '../assets/logo.svg'
+
+export default function AddTokenPage() {
+  return (
+    <div className="add-token-page-content">
+      <LogoIcon className="icon-logo"/>
+      <div className="wish-wallet">
+        <StarIcon className="star-icon" fill="#EEFB40"/>
+        <h2>Wish Wallet</h2>
+      </div>
+      <div className='add-token-tittle'>
+        <h1>Add Token</h1>
+        <BackButton />
+      </div>
+      <Token />
+    </div>
+  )
+}
